eb5172bed1df61c68c9e27ce03fc52ecddf6d997
totaotata <totaotata@protonmail.com>
2019-01-17 21:38:04 +0100

    Jeudi OK

--- lib/deputies_scrapper.rb
+++ lib/deputies_scrapper.rb
@@ -1,9 +1,11 @@
-
-
+# Exercice THP liste des députés
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
+require 'rubygems'
 
-
+# metode return_url permettant de recuperer tout les liens des députes de la liste
+# et de stocke les données dans un array url
 def return_url
   url = []
   doc = Nokogiri::HTML(open('https://www.nosdeputes.fr/deputes'))
@@ -12,25 +14,33 @@ def return_url
   end
   return url
 end
+
+# methode deputy_info permettant de récupérer lpour chaque lien passe en parmatre
+# l'email et premnom nom, puis de partager prenom nom en 2 variables prénom et nol
+# et de stocker toutes ces infos dans un hash liste_deputes
 def deputy_info(urls)
-  list_deputes = []
-  urls.each  do |url|
-      hash = {}
-  doc = Nokogiri::HTML(open("https://www.nosdeputes.fr#{url}"))
-  name_depute = (doc.xpath('//h1').text.split())
-  hash[:first_name] = name_depute[0]
-  hash[:list_name] = name_depute[1]
-  hash[:email] =  doc.xpath('//*[@id="b1"]/ul[2]/li[1]/ul/li[1]/a').text
-  list_deputes << hash
+  liste_deputes = []
+  urls.each do |url|
+    hash = {}
+    doc = Nokogiri::HTML(open("https://www.nosdeputes.fr#{url}"))
+    name_depute =  (doc.xpath('//h1').text.split())
+    hash[:first_name] = name_depute[0]
+    hash[:last_name] =  name_depute[1]
+    hash[:email] =  doc.xpath('//*[@id="b1"]/ul[2]/li[1]/ul/li[1]/a').text
+    liste_deputes << hash
   end
-  return list_deputes
+  return liste_deputes
 end
 
+
+#binding.pry
+
 def perform
-puts list_final = deputy_info(return_url)
-fname = "liste deputes.txt"
-somefile = File.open(fname, "w")
-somefile.puts(list_final)
-somefile.close
+  puts list_final = deputy_info(return_url)
+  fname = "liste_deputes.txt"
+  somefile = File.open(fname,"w")
+  somefile.puts(list_final)
+  somefile.close
 end
+
 perform
